9a71f26068069ac8e4b050c301eb54b8286e297c
Sreenath <absreenath212436@gmail.com>
2025-09-13 16:56:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("root").innerHTML = `
+  <h1>Portfolio Placeholder</h1>
+  <p>This is where your React app will render once built with Vite/React.</p>
+  <p>If you want the animated avatar and full features, you must run \`npm run build\` in your React project and deploy the <code>dist</code> folder.</p>
+`;
