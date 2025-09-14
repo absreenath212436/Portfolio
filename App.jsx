4c7cf667bd7aefb51d29b1f665af8800299ba038
Sreenath <absreenath212436@gmail.com>
2025-09-13 18:11:27 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PortfolioWithAvatar from './PortfolioWithAvatar'; // Adjust path if needed
+
+export default function App() {
+  return (
+    <div>
+      <PortfolioWithAvatar />
+    </div>
+  );
+}
